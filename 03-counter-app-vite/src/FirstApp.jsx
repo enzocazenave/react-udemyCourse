43ecd163d56114ebc685807b827eb3ab91eb7cbc
enzocazenave <encaze24@hotmail.com>
2022-06-30 01:39:01 -0300

    First components in Vite React

---
+++ 03-counter-app-vite/src/FirstApp.jsx
@@ -0,0 +1,10 @@
+const newMessage = ["Enzo", "Gustavo", "Boris"];
+
+export const FirstApp = () => {
+    return (
+        <>
+            <h1>{ newMessage }</h1>
+            <p>Soy un subtitulo</p>
+        </>
+    );
+}
