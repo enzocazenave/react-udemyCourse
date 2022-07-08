9c482a0e716d8753c04c5659c8bf5ebdfc5e3807
enzocazenave <encaze24@hotmail.com>
2022-07-08 17:17:36 -0300

    Add test.js

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { FirstApp } from './FirstApp'; 
+//import { FirstApp } from './FirstApp'; 
 import { CounterApp } from './CounterApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
-        <FirstApp title="Hola, soy Enzo"/>
         <CounterApp value={0}></CounterApp>
     </React.StrictMode>
 )
