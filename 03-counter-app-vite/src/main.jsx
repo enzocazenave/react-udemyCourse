8a325fd248bf0f41d7806e45a3903e394e3be724
enzocazenave <encaze24@hotmail.com>
2022-07-08 01:27:12 -0300

    Add new component

--- 03-counter-app-vite/src/main.jsx
+++ 03-counter-app-vite/src/main.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { FirstApp } from './FirstApp'; 
+import { CounterApp } from './CounterApp';
 import './styles.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <FirstApp title="Hola, soy Enzo"/>
+        <CounterApp value={0}></CounterApp>
     </React.StrictMode>
 )
