35bcf8fe0be7653dc7f6036b2b88a7597fe6ca6c
enzocazenave <encaze24@hotmail.com>
2022-07-27 04:45:06 -0300

    Save lastPath and public routes

--- heroes-spa/src/auth/pages/LoginPage.jsx
+++ heroes-spa/src/auth/pages/LoginPage.jsx
@@ -1,13 +1,20 @@
+import { useContext } from 'react';
 import { useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context';
 
 export const LoginPage = () => {
 
+    const { login } = useContext(AuthContext);
     const navigate = useNavigate();
 
     const onLogin = () => {
-        navigate('/', {
+        const lastPath = localStorage.getItem('lastPath') || '/';
+
+        login('Enzo Cazenave');
+
+        navigate(lastPath, {
             replace: true
-        })
+        });
     }    
 
     return (
--- heroes-spa/src/ui/components/Navbar.jsx
+++ heroes-spa/src/ui/components/Navbar.jsx
@@ -1,7 +1,11 @@
+import { useContext } from 'react';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../auth';
 
 
 export const Navbar = () => {
+
+    const { user, logout } = useContext(AuthContext)
 
     const navigate = useNavigate();
 
@@ -9,6 +13,7 @@ export const Navbar = () => {
         navigate('/login', {
             replace: true
         });
+        logout();
     }
 
     return (
@@ -50,7 +55,7 @@ export const Navbar = () => {
             <div className="navbar-collapse collapse w-100 order-3 dual-collapse2 d-flex justify-content-end">
                 <ul className="navbar-nav ml-auto">
                     <span className='nav-item nav-link text-info'>
-                        Enzo
+                        {user?.name}
                     </span>
 
                     <button
